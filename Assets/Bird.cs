de0b1c444923c2e676f5acba08f18d7a01be82f0
loveiset <loveiset@qq.com>
2014-05-14 18:38:26 +0800

    增加鸟的运动（下落和转方向）

---
+++ Assets/Bird.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour {
+    Vector3 velocity = Vector3.zero;
+    public Vector3 gravity = Vector3.zero;
+    public Vector3 flyVelocity = Vector3.zero;
+    public float maxSpeed = 5f;
+    bool isFly = false;
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isFly = true;
+            Debug.Log(transform.rotation);
+            Debug.Log(velocity.y);
+        }
+	}
+
+    void FixedUpdate()
+    {
+        velocity += gravity * Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;
+        if (isFly)
+        {
+            isFly = false;
+
+            velocity = flyVelocity;
+        }
+
+        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+
+        float angle = 10;
+        if (velocity.y < -1f)
+        {
+            angle = Mathf.Lerp(10, -90, -velocity.y / maxSpeed);
+        }
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+
+    }
+}
---
+++ Assets/Wall.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour {
+    public Vector3 velocity = Vector3.zero;
+
+	// Use this for initialization
+	void Start () 
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.position += velocity * Time.deltaTime;
+	}
+
+
+}
